1a8552f6abb504e5b9e1273b657240eacb08e573
Paul Mason <latepaul@gmail.com>
2014-10-19 23:28:59 +0100

    Final version

--- memory/memory.py
+++ memory/memory.py
@@ -1,5 +1,5 @@
 # implementation of card game - Memory
-# Codeskulptor URL:
+# Codeskulptor URL: http://www.codeskulptor.org/#user38_KbdNPJK9n69jrwe.py
 
 # allow use in CodeSkulptor or regular python with simpleguitk
 try:
@@ -23,12 +23,16 @@ else:
 cards = list(range(8))
 cards.extend(list(range(8)))
 exposed = []
+im = []
 for c in range(16):
     exposed.append(False)
 
 state = 0
 card1 = card2 = -1
 turns = 0
+PICTURES = False
+NUMBERS = True
+card_set = NUMBERS
 
 # helper function to initialize globals
 def new_game():
@@ -43,6 +47,18 @@ def new_game():
 
     label.set_text("Turns = "+str(turns))
 
+def switch_sets():
+    global card_set, sw_button
+    
+    if card_set == PICTURES:
+        card_set = NUMBERS
+        sw_button.set_text("Pictures")
+    else:
+        card_set = PICTURES
+        sw_button.set_text("Numbers")
+        
+    new_game()
+    
 # define event handlers
 def mouseclick(pos):
     # add game state logic here
@@ -72,22 +88,31 @@ def mouseclick(pos):
 
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
-    global cards, im,loaded
+    global cards, im,loaded, card_set
 
+    if card_set == PICTURES and not loaded:
+        canvas.draw_text("PLEASE WAIT WHILE IMAGES LOAD",[10,50],CARD_FONT,"Orange")
+        return
+        
     for card in range(len(cards)):
         if exposed[card]:
-            canvas.draw_polygon([[card*50,0],[(card+1)*50,0],[(card+1)*50,100],[card*50,100]],2,"White","Black")
-            canvas.draw_text(str(cards[card]),[card*50+CARD_XPOS,CARD_YPOS],CARD_FONT,"White")
+            if card_set == NUMBERS:
+                canvas.draw_polygon([[card*50,0],[(card+1)*50,0],[(card+1)*50,100],[card*50,100]],2,"White","Black")
+                canvas.draw_text(str(cards[card]),[card*50+CARD_XPOS,CARD_YPOS],CARD_FONT,"White")
+            else:
+                canvas.draw_image(im[cards[card]],[44,60],[88,120],[card*50+25,50],[50,100])
         else:
-            canvas.draw_polygon([[card*50,0],[(card+1)*50,0],[(card+1)*50,100],[card*50,100]],2,"White","Green")
-
-        if loaded:
-            canvas.draw_image(im,[100,125],[200,250],[card*50+25,50],[50,100])
+            if card_set == NUMBERS:
+                canvas.draw_polygon([[card*50,0],[(card+1)*50,0],[(card+1)*50,100],[card*50,100]],2,"White","Green")
+            else:
+                canvas.draw_image(back_im,[44,60],[88,120],[card*50+25,50],[50,100])
+                
 
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
 frame.add_button("Reset", new_game)
 label = frame.add_label("Turns = 0")
+sw_button = frame.add_button("Pictures", switch_sets)
 
 # register event handlers
 frame.set_mouseclick_handler(mouseclick)
@@ -95,7 +120,33 @@ frame.set_draw_handler(draw)
 
 # get things rolling
 loaded=False
-im = simplegui.load_image("http://images.all-free-download.com/images/graphiclarge/spade_suit_two_playing_cards_311648.jpg")
+
+back_im=simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmWFV1UFFYRWswdDg")
+#jacks
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmQzRRVEV1S3lOZmc"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmbU9EQWhSUzh6TXc"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmNWVheFJBUkZPZE0"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmYUhJSWY4TlRmR0k"))
+
+#queens
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmTDdBaHRTNzRDTXM"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmVlZ0MEsyVzN2U0U"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmaW9pcmdKaW1IeGM"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmRVdPeGVjOXhGT00"))
+
+#kings
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmWjh5anlXam4wd2s"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmaFRFbzRicHc3RDA"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmOXlCdG85S3dhUVU"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmcUlCRnAtUWREamc"))
+
+#aces
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmRzI4MnpCdC1GYlU"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmdGU4N1NKa1kyalk"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmbDJ5eG44TnJBQ0U"))
+im.append(simplegui.load_image("https://docs.google.com/uc?export=download&id=0B4HFB7ccwbPmWkotYVBHTEdILWs"))
+
+random.shuffle(im)
 loaded=True
 
 new_game()
